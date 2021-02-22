cfbac3a1549e3d18ea4134d973ea398001b3c31e
Торадзе Диана <dianatoradze@gmail.com>
2021-02-22 22:14:17 +0500

    Add Task1 solution finish

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -4,7 +4,7 @@ public class Task4 {
     public static void main(String[] args) {
         int i=1980;
         while(i<=2020) {
-            System.out.println("Олимпиада " + i + " года ");
+            System.out.println("Олимпиада" +" "+ i + " " + "года");
             i=i+4;
         }
     }
