8950b7f1f5ff4bfac9a65f2e06ff039742260505
Торадзе Диана <dianatoradze@gmail.com>
2021-02-23 21:51:13 +0500

    Add Task2 solution

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner stage = new Scanner(System.in);
+        //System.out.println("Введите количество этажей в здании");
+        int x = stage.nextInt();
 
+        if (x >= 1 && x <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (x >= 5 && x <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (x >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else if (x <= 0) {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
+
+
