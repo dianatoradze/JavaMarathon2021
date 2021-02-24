3eb516c038e161e98c59ca7b4dbd1296174822d2
Торадзе Диана <dianatoradze@gmail.com>
2021-02-25 00:24:54 +0500

    Add Task2 solution

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,43 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; scanner.hasNextLine(); i++) {
+            switch (scanner.nextLine()) {
+                case "Москва":
+                case "Ростов":
+                case "Владивосток":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                case "Stop":
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
 
+//                    if (scanner.nextLine().equals("Stop")) {
+//                        break;
+//                    }
+            }
+        }
     }
 }
+
+
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; scanner.hasNextDouble(); i++) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            boolean error = false;
+            if (b == 0) {
+                error = true;
+                break;
+            } else {
+                double result = a / b;
+                System.out.println(result);
+            }
+        }
     }
 }
